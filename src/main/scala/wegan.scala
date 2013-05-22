9b6a7ed1785aa9830b274d1c3ac60543346678b3
Lars Yencken <lars@yencken.org>
2013-05-22 17:30:52 +1000

    Turn wegan into a passable command-line app.

--- src/main/scala/wegan.scala
+++ src/main/scala/wegan.scala
@@ -4,7 +4,25 @@ import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.remote.{DesiredCapabilities,CapabilityType}
 
 object Wegan {
-  def main(args: Array[String]) = {
+  def usage(): Unit = {
+    println("Usage: wegan url output.har")
+    println("")
+    println("Load the url in chrome and save the performance data as a HAR file.")
+  }
+
+  def main(args: Array[String]): Unit = {
+    if (args.length != 2) {
+      usage()
+      return
+    }
+    val url = args(0)
+    val dest = args(1)
+
+    performanceTest(url, dest)
+  }
+
+  def performanceTest(url: String, dest:String): Unit = {
+    println("Starting proxy server")
     val server = new ProxyServer(9090)
     server.start()
 
@@ -13,14 +31,20 @@ object Wegan {
     val capabilities = new DesiredCapabilities();
     capabilities.setCapability(CapabilityType.PROXY, proxy)
 
+    println("Starting Chrome driver")
     val driver = new ChromeDriver(capabilities)
-    server.newHar("99designs.com")
+    server.newHar("misc-site")
 
-    driver.get("http://99designs.com.au")
+    println("Fetching " + url)
+    driver.get(url)
 
+    println("Saving data to " + dest)
     val har = server.getHar()
-    har.writeTo(new File("99designs-com.har"));
-    driver.close
+    har.writeTo(new File(dest))
+
+    println("Closing driver")
+    //driver.close()  // XXX hangs
+    exit(0)
   }
 }
 
